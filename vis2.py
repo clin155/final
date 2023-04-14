a79ae315fa0fc4c47c5d1b2f3e6303d66c96506b
David Diokno <dadiokno@umich.edu>
2023-04-14 14:19:52 -0400

    test

--- vis2.py
+++ vis2.py
@@ -3,10 +3,43 @@ import matplotlib.pyplot as plt
 import config
 import sqlite3
 
-def genre_rev():
+def pokemon_pie():
     conn = config.getdb()
     cur = conn.cursor()
 
-    cur.execute('''
-        SELECT g.name, AVG 
-    ''')
+    cur.execute("SELECT type, COUNT(*) FROM pokemon GROUP BY type")
+    data = cur.fetchall()
+    types, counts = zip(*data)
+
+    type_colors = {
+        'bug': '#A6B91A',
+        'dark': '#705746',
+        'dragon': '#6F35FC',
+        'electric': '#F7D02C',   
+        'fairy': '#D685AD',
+        'fighting': '#C22E28',
+        'fire': '#EE8130',
+        'flying': '#A98FF3',
+        'ghost': '#735797',
+        'grass': '#7AC74C',
+        'ground': '#E2BF65',
+        'ice': '#96D9D6',
+        'normal': 'beige',
+        'poison': '#A33EA1',
+        'psychic': '#F95587',
+        'rock': '#B6A136',
+        'steel': '#B7B7CE',
+        'water': '#6390F0'
+    }
+
+    colors = [type_colors[type] for type in types]
+
+    plt.figure(figsize =(8,8))
+    plt.pie(counts, labels=types, colors=colors, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.title('Pie Chart of Pokemon Types')
+    plt.show()
+
+    config.closedb(conn)
+
+pokemon_pie()
