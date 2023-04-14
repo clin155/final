09b4d723fc18b1e2d9411dec9381a9650de3c798
David Diokno <dadiokno@umich.edu>
2023-04-14 16:13:28 -0400

    changed a txt file name

--- calc.py
+++ calc.py
@@ -33,7 +33,7 @@ def avg_stats_by_type():
     ''')
     results = cur.fetchall()
 
-    with open("avg_stats_by_type.txt", "w") as file:
+    with open("avg_stats_by_poketype.txt", "w") as file:
         for _type, avg_attack, avg_defense, avg_speed in results:
             file.write(f"Average stats for {_type} Pokemon: Attack {avg_attack:.2f}, Defense {avg_defense:.2f}, Speed {avg_speed:.2f}\n")
 
