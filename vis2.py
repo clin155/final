e4132832bddacf26e930c022e52fb395a91f8c5f
David Diokno <dadiokno@umich.edu>
2023-04-14 10:32:07 -0400

    pull time

--- vis2.py
+++ vis2.py
@@ -3,3 +3,10 @@ import matplotlib.pyplot as plt
 import config
 import sqlite3
 
+def genre_rev():
+    conn = config.getdb()
+    cur = conn.cursor()
+
+    cur.execute('''
+        SELECT g.name, AVG 
+    ''')
